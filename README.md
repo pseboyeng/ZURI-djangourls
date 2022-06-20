9db2fa7109f69b110cd578b981d3a3042ce024e2
pseboyeng@gmail.com <pseboyeng@gmail.com>
2022-06-20 13:47:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+go to http://127.0.0.1:8000/zuri-admin
+
+username = peter
+password = peter
---
+++ peter_seboyeng/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for peter_seboyeng project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peter_seboyeng.settings')
+
+application = get_wsgi_application()
